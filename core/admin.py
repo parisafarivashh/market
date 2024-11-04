fbed1a84750b9a99f59d33998e1eefb41cec8862
parisa <parisafarivash@gmail.com>
2024-11-04 11:34:48 +0330

    Implemented custom admin model

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,28 @@
 from django.contrib import admin
+from django.contrib.admin import ModelAdmin, StackedInline
 
-# Register your models here.
+from core.models import Category, Product, Variant
+
+
+class VariantInline(StackedInline):
+    model = Variant
+
+
+class ProductAdmin(ModelAdmin):
+    list_display = ['id', 'title', 'creator__title', 'category__title']
+    list_filter = ['id', 'title', 'creator__title', 'category__title']
+    readonly_fields = ['id', 'creator', 'category']
+    list_select_related = ['creator', 'category']
+    ordering = ['id']
+    inlines = [VariantInline]
+
+
+class CategoryAdmin(ModelAdmin):
+    list_display = ['id', 'title']
+    list_filter = ['id', 'title']
+    readonly_fields = ['id']
+    ordering = ['id']
+
+
+admin.site.register(Product, ProductAdmin)
+admin.site.register(Category, CategoryAdmin)
